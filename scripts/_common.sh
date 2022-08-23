45470821dc39eb78c48e44126288e8ca25aa9219
Thomas Sileo <t@a4.io>
2022-08-23 19:14:06 +0200

    Improve backup

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,28 +13,43 @@ pkg_dependencies="python3 python3-dev libxml2-dev libxslt-dev gcc libjpeg-dev zl
 python_version=3.10.6
 
 microblogpub_install_python () {
-    # TODO: delete old Python version
-    local final_path="$1"
+    local final_path="opt/yunohost/${YNH_APP_INSTANCE_NAME}"
+    # Install/update pyenv
+    ynh_setup_source --dest_dir="$final_path/.pyenv" --source_id=pyenv
+    export PYENV_ROOT=${final_path}/pyenv
+
+    old_python_version=$(ynh_app_setting_get --app=$YNH_APP_INSTANCE_NAME --key=python_version)
+    if [ -z "${old_python_version}" ]; then
+        if [ "${old_python_version}" != "${python_version}" ]; then
+            old_python_version_path="${final_path}/pyenv/versions/${old_python_version}"
+            if [ -d "${old_python_version_path}" ]; then
+                ynh_print_info --message="Deleting Python ${old_python_version}"
+                rm -rf $old_python_version_path
+            fi
+        fi
+    fi
+
     if [ ! -d "${final_path}/pyenv/versions/${python_version}" ]; then
         ynh_print_info --message="Installing Python ${python_version}"
-        export PYENV_ROOT=${final_path}/pyenv
-        ${final_path}/.pyenv/bin/pyenv install $python_version
-    else
-        ynh_print_info --message="Python ${python_version} already installed"
+        $final_path/.pyenv/bin/pyenv install $python_version
     fi 
 }
 
 microblogpub_install_deps () {
-    local final_path="$1"
-    export PATH="${final_path}/pyenv/versions/${python_version}/bin:$PATH"
-    pip install poetry
-    export POETRY_VIRTUALENVS_PATH=${final_path}/venv
-    cd ${final_path}/microblogpub
-    poetry install
+    ynh_print_info --message="Installing deps with poetry"
+    local final_path="opt/yunohost/${YNH_APP_INSTANCE_NAME}"
+    (
+        export PATH="${final_path}/pyenv/versions/${python_version}/bin:$PATH"
+        pip install poetry
+        export POETRY_VIRTUALENVS_PATH=${final_path}/venv
+        cd ${final_path}/microblogpub
+        poetry install
+    )
 }
 
 microblogpub_update () {
-    local final_path="$1"
+    ynh_print_info --message="Updating microblogpub"
+    local final_path="opt/yunohost/${YNH_APP_INSTANCE_NAME}"
     (
         export PATH="${final_path}/pyenv/versions/${python_version}/bin:$PATH"
         cd ${final_path}/microblogpub
