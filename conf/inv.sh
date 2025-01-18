c983a286a748bfcd236970712bed251e664f33e7
yalh76 <yalh@yahoo.com>
2025-01-18 17:32:51 +0100

    fix missing inv.sh

--- conf/inv.sh
+++ conf/inv.sh
@@ -2,7 +2,7 @@
 
 cmd=$1
 (
-    export PATH="__INSTALL_DIR__/pyenv/versions/__PYTHON_VERSION__/bin:$PATH"
+    export PATH=__PATH_WITH_PYTHON__
     cd "__INSTALL_DIR__/app"
     export POETRY_VIRTUALENVS_PATH="__INSTALL_DIR__/venv"
     poetry run inv $cmd
